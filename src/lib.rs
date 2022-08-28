30b30a4279fbc0c28851df4287db922ccda5c091
lemonadern <taishi.develop@gmail.com>
2022-08-28 17:26:01 +0900

    Initial Commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+extern crate speculate;
+
+mod tests {
+    use speculate::speculate;
+
+    speculate! {
+        describe "add 関数" {
+            it "1 + 1 = 2" {
+                assert_eq!(1+1,2)
+            }
+            
+            #[should_panic]
+            it "パニックする" {
+                assert_eq!(1+1 , 3)
+            }
+        }
+    }
+}
