78c57951bdeb5b4fcf39d988832f044e2535a7f6
lemonadern <taishi.develop@gmail.com>
2022-08-31 06:34:57 +0900

    refactor rotor and make new() to Result

--- src/rotor.rs
+++ src/rotor.rs
@@ -1,23 +1,36 @@
-use std::collections::HashMap;
+use std::{collections::HashMap,io::{Error, ErrorKind}};
 
 use crate::mod26::Mod26;
 
+type Position = Mod26;
+type Mod26Map = HashMap<Position, Position>;
+
 pub struct Rotor {
     offset: Mod26,
-    forward_map: HashMap<Mod26, Mod26>,
-    reverse_map: HashMap<Mod26, Mod26>,
+    forward_map: Mod26Map,
+    reverse_map: Mod26Map,
 }
 
-type Position = Mod26;
-
 impl Rotor {
-    pub fn new(offset: Mod26, map: HashMap<Mod26, Mod26>) -> Self {
-        let reverse_map = map.iter().map(|(&k, &v)| (v, k)).collect();
-        Self {
+    pub fn new(offset: Mod26, map: Mod26Map) -> Result<Self, Error> {
+        if map.len() != 26 {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "map length must be 26",
+            ));
+        }
+        let reverse_map: HashMap<Mod26,Mod26> = map.iter().map(|(&k, &v)| (v, k)).collect();
+        if reverse_map.len() != 26 {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                "map value duplicated",
+            ));
+        }
+        Ok(Self {
             offset,
             forward_map: map,
             reverse_map,
-        }
+        })
     }
 
     pub fn increment_offset(&mut self) {
@@ -26,19 +39,12 @@ impl Rotor {
 
     pub fn substitute_from_forward(&self, input_position: Position) -> Position {
         let input = input_position + self.offset;
-        /*
-        todo: unwrapの扱い 要検討
-        */
-        self.forward_map.get(&input).cloned().unwrap_or(input) - self.offset
+        self.forward_map.get(&input).cloned().unwrap() - self.offset
     }
 
     pub fn substitute_from_backward(&self, input_position: Position) -> Position {
         let input = input_position + self.offset;
-        /*
-        todo: unwrapの扱い 要検討
-        */
- 
-        self.reverse_map.get(&input).cloned().unwrap_or(input) - self.offset
+        self.reverse_map.get(&input).cloned().unwrap() - self.offset
     }
 }
 
@@ -47,18 +53,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn rotorの初期化() {
+    fn 正常系() {
         let offset = Mod26::new(0);
         let mut map = HashMap::new();
         // (i, i+2) と対応するようなHashMap
         for i in 0..26 {
             map.insert(Mod26::new(i), Mod26::new(i + 2));
         }
-        let mut rotor = Rotor::new(offset, map);
+        let mut rotor = Rotor::new(offset, map).unwrap();
 
-        let map_length = rotor.forward_map.len();
-        // HashMap の要素が26個
-        assert_eq!(map_length, 26);
 
         let zero = Mod26::new(0);
         let one = Mod26::new(1);
@@ -74,21 +77,25 @@ mod tests {
     }
 
     #[test]
-    fn offsetをインクリメントできる() {
-        let zero = Mod26::new(0);
-        let one = Mod26::new(1);
-        let map = HashMap::new();
-        let mut rotor = Rotor::new(zero, map);
-        rotor.increment_offset();
-        // 0 -> 1 になる
-        assert_eq!(rotor.offset, one);
+    fn mapの要素数が25のとき初期化に失敗する() {
+        let offset = Mod26::new(0);
+        let mut map = HashMap::new();
+        for i in 0..25 {
+            map.insert(Mod26::new(i), Mod26::new(i + 2));
+        }
+        let rotor = Rotor::new(offset, map);
+        assert!(rotor.is_err());
+    }
 
-        let twenty_five = Mod26::new(25);
-        let map = HashMap::new();
-        let mut rotor = Rotor::new(twenty_five, map);
-        rotor.increment_offset();
-        // 25 -> 0 になる
-        assert_eq!(rotor.offset, zero);
+    #[test]
+    fn valueに重複があるとき初期化に失敗する() {
+        let offset = Mod26::new(0);
+        let mut map = HashMap::new();
+        for i in 0..26 {
+            map.insert(Mod26::new(i), Mod26::new(0));
+        }
+        let rotor = Rotor::new(offset, map);
+        assert!(rotor.is_err());
     }
 
     #[test]
@@ -101,7 +108,7 @@ mod tests {
             map.insert(Mod26::new(i), Mod26::new(i + 1));
         }
 
-        let rotor = Rotor::new(zero, map);
+        let rotor = Rotor::new(zero, map).unwrap();
 
         // 前から換字する
         assert_eq!(rotor.substitute_from_forward(zero), one);
@@ -120,7 +127,7 @@ mod tests {
         for i in 0..26 {
             map.insert(Mod26::new(i), Mod26::new(i + 1));
         }
-        let rotor = Rotor::new(two, map);
+        let rotor = Rotor::new(two, map).unwrap();
 
         //     offset = 2
         //     map = i -> i + 1
